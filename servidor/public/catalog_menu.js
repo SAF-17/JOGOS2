a1bb0b7ec7e34fb08ce1ceeb9d28829db03225d7
SAF-17 <121833714+SAF-17@users.noreply.github.com>
2024-01-11 15:27:32 +0000

    a

--- servidor/public/catalog_menu.js
+++ servidor/public/catalog_menu.js
@@ -6,7 +6,7 @@ const cardHeight = 180; // Altura de cada carta
 let imagens = []; // Array para armazenar as imagens
 
 function catalogIU() {
-  buttonLoja();
+  buttonLoja2();
 
   cardAreaWidth = width / 10;
   textSize(24);
@@ -29,20 +29,20 @@ function catalogIU() {
         const row = floor(i / cardsPerRow);
         const col = i % cardsPerRow;
         const x = 50 + col * (cardWidth + 20);
-        const y = 50 + row * (cardHeight + 20);
+        const y = 100 + row * (cardHeight + 20);
 
         // Desenha quadrado da carta
-        fill(200);
-        if (mouseIsOverRect(x, y, cardWidth, cardHeight)) {
+       
           fill(220); // Altera a cor quando o mouse está sobre a carta
+          
           displayCardDetails(carta, x, y, cardWidth, cardHeight);
-        }
+       
         rect(x, y, cardWidth, cardHeight);
 
         // Carrega a imagem dinamicamente durante o loop
         loadImage(`/img/${carta.nome_carta}.png`, (img) => {
           // Exibe a imagem da carta
-          image(img, x, y, cardWidth, cardHeight / 2);
+          image(img, x, y, cardWidth, cardHeight);
         });
       }
     } else {
@@ -57,16 +57,26 @@ function catalogIU() {
 
 // Função para exibir os detalhes da carta
 function displayCardDetails(carta, x, y, w, h) {
+  fill(150);
+  rect(x, y +cardHeight, cardWidth, cardHeight/2);
   textSize(12);
   textAlign(LEFT, TOP);
   fill(0);
-  text(`Nome: ${carta.nome_carta}`, x, y + h  + 10);
-  text(`Attack: ${carta.attack_carta}`, x, y + h + 30);
-  text(`Defesa: ${carta.defend_carta}`, x, y + h  + 50);
+  text(`Nome: ${carta.nome_carta}`, x+10, y + h  + 10);
+  text(`Attack: ${carta.attack_carta}`, x+10, y + h + 30);
+  text(`Defesa: ${carta.defend_carta}`, x+10, y + h  + 50);
   // Adapte conforme necessário para exibir outras informações da carta
 }
 
-// Função para verificar se o mouse está sobre um retângulo
-function mouseIsOverRect(x, y, w, h) {
-  return mouseX >= x && mouseX <= x + w && mouseY >= y && mouseY <= y + h;
+
+function buttonLoja2(){
+
+  lojaBtn = createButton('Loja');
+  lojaBtn.position(centroVertical+distanciaButtonVertical,  distanciaButtonHorizontal);
+  lojaBtn.style('background-color', '#a87a63');
+  lojaBtn.style('border', 'none');
+  lojaBtn.style('padding', '10px 20px');
+  lojaBtn.mousePressed(LojaScene);
+
+
 }
